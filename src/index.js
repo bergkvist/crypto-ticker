e802c5892e0ae932eb98072c15dd63d4eb5c3668
Tobias Bergkvist <tobias@bergkv.ist>
2019-02-06 15:53:33 +0100

    Add usage on no arguments

--- src/index.js
+++ src/index.js
@@ -10,13 +10,28 @@ const cryptocompare = Axios.create({
     timeout: 2000
 })
 
-cryptocompare.get(`price?fsym=${base}&tsyms=${quote}`)
-    .then(response => {
-        const price = response.data[quote]
-        if (price !== undefined) {
-            console.log(price)
-        } else {
-            console.log(response.data.Message) /* Error message */
-        }
-    })
-    .catch(error => console.log('N/A') /* Timed out */)
+if (base === undefined) {
+    // Usage/help
+    console.log(
+        'Usage: crypto-ticker [base] [quote (default=USD)]',
+        '\n',
+        '\nwhere',
+            '\n\t[base] refers to the base currency (case insensitive).',
+            '\n\t[quote] refers to quote currency (case insensitive. USD if unspecified).',
+        '\n',
+        '\nexample:',
+        '\n\t$ crypto-ticker BTC EUR',
+        '\n\t3435.31\n'
+    )
+} else {  
+    cryptocompare.get(`price?fsym=${base}&tsyms=${quote}`)
+        .then(response => {
+            const price = response.data[quote]
+            if (price !== undefined) {
+                console.log(price)
+            } else {
+                console.log(response.data.Message) /* Error message */
+            }
+        })
+        .catch(error => console.log('N/A') /* Timed out */)
+}
